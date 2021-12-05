bc400671e09de532762d91c63edb6e727c3008c8
Simon Farre <simon.farre.cx@gmail.com>
2021-12-05 13:31:57 +0100

    Removed the override of the previously virtual method ToLibraryItem (it is no longer virtual)

--- src/web/mvc/Models/ViewModels/EditLibraryItemViewModel.cs
+++ src/web/mvc/Models/ViewModels/EditLibraryItemViewModel.cs
@@ -4,14 +4,6 @@ namespace webapp.mvc.Models.ViewModels;
 public class EditLibraryItemModel : CreateLibraryItemModel {
     public int ID { get; set; }
 
-    public override LibraryItem? ToLibraryItem() {
-        var item = base.ToLibraryItem();
-        if (item != null) {
-            ID = item.ID;
-        }
-        return item;
-    }
-
     public bool? IsBorrowable { get; set; }
     public string? Borrower { get; set; }
     public DateTime? BorrowDate { get; set; }
