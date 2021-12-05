bf3f7a1ce6feae72497d8753ff3239536995134d
Simon Farre <simon.farre.cx@gmail.com>
2021-12-05 13:24:46 +0100

    removed dead code

--- src/web/mvc/Models/LibraryItemListInterface.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace webapp.mvc.Models;
-
-public abstract class ILibraryItem {
-    public abstract string Listing { get; }
-    public abstract string GetTitle();
-}
